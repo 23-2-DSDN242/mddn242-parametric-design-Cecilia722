8089c032271931fe268e298862d6db739483acf3
Cecilia722 <114236944+Cecilia722@users.noreply.github.com>
2023-09-15 17:29:26 +1200

    1

--- letters.js
+++ letters.js
@@ -5,20 +5,21 @@ const alphabet = {
   
     "offsetx": 0,
     "offsety": 0,
+    "rotate":0*PI/2,
 
     "x_2":0,
     "y_2":0,
-    "size3":0,
-    "size_v3":0
+    "size3":0
+  
     
   },
  
   "A": {
-    "size": 80,
-    "size_v":20,
+   
+    
+
   
-    "offsetx": -100,
-    "offsety": -100
+    
   
   },
 
